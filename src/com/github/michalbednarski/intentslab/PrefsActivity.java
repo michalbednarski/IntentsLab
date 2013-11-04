73344657e502ec82728de5c5f986a2a05d3105ab
Michał Bednarski <bednarski.michal2@gmail.com>
2013-11-04 12:14:31 +0100

    Remove unused method

--- src/com/github/michalbednarski/intentslab/PrefsActivity.java
+++ src/com/github/michalbednarski/intentslab/PrefsActivity.java
@@ -1,15 +1,9 @@
 package com.github.michalbednarski.intentslab;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class PrefsActivity extends PreferenceActivity {
-	static SharedPreferences getPrefs(Context ctx) {
-		return ctx.getSharedPreferences("intenteditor", MODE_PRIVATE);
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
