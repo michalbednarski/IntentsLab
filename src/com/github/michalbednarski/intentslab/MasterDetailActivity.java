300b84d46653b7a40402cef0cd006b1f74a7ba2c
Michał Bednarski <bednarski.michal2@gmail.com>
2014-02-01 17:05:19 +0100

    Move master-detail specific code from BrowseComponentsActivity to superclass, better handling of tablets in portrait

---
+++ src/com/github/michalbednarski/intentslab/MasterDetailActivity.java
@@ -0,0 +1,93 @@
+package com.github.michalbednarski.intentslab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+/**
+ * Base activity implementing master-detail pattern
+ */
+public abstract class MasterDetailActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.master_detail_base);
+
+        boolean showDetail = false;
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.master, createMasterFragment()).commit();
+        } else {
+            Fragment detail = getDetail();
+            if (detail != null) {
+                if (!usingTabletView()) {
+                    // Device changed orientation and is not using master-detail anymore
+                    // Hide master
+                    findViewById(R.id.master).setVisibility(View.GONE);
+                }
+                showDetail = true;
+            }
+        }
+        findViewById(R.id.detail).setVisibility(showDetail ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * Create fragment for use as master view
+     */
+    protected abstract Fragment createMasterFragment();
+
+    private Fragment getDetail() {
+        return getSupportFragmentManager().findFragmentById(R.id.detail);
+    }
+
+    private boolean usingTabletView() {
+        return getResources().getBoolean(R.bool.use_master_detail);
+    }
+
+    /**
+     * Open given fragment in detail panel or new activity
+     */
+    public void openFragment(Class<? extends Fragment> fragmentClass, Bundle arguments) {
+        if (usingTabletView()) {
+            // Put in detail fragment
+            Fragment fragment;
+            try {
+                fragment = fragmentClass.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            fragment.setArguments(arguments);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.detail, fragment)
+                    .commit();
+            findViewById(R.id.detail).setVisibility(View.VISIBLE);
+        } else {
+            // Just open in new activity
+            arguments.putString(SingleFragmentActivity.EXTRA_FRAGMENT, fragmentClass.getName());
+            startActivity(new Intent(this, SingleFragmentActivity.class).replaceExtras(arguments));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!usingTabletView() && getDetail() != null) {
+            closeDetail();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void closeDetail() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .remove(getDetail())
+                .commit();
+        findViewById(R.id.detail).setVisibility(View.GONE);
+        findViewById(R.id.master).setVisibility(View.VISIBLE);
+    }
+}
--- src/com/github/michalbednarski/intentslab/browser/BrowseComponentsActivity.java
+++ src/com/github/michalbednarski/intentslab/browser/BrowseComponentsActivity.java
@@ -1,17 +1,13 @@
 package com.github.michalbednarski.intentslab.browser;
 
 import android.annotation.TargetApi;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.MenuItem;
-import android.view.View;
-import com.github.michalbednarski.intentslab.R;
-import com.github.michalbednarski.intentslab.SingleFragmentActivity;
+import com.github.michalbednarski.intentslab.MasterDetailActivity;
 
-public class BrowseComponentsActivity extends FragmentActivity {
+public class BrowseComponentsActivity extends MasterDetailActivity {
 
     public static final String EXTRA_FETCHER = "BrowseComponentsActivity.fetcher";
     public static final String EXTRA_SERIALIZED_FETCHER = "BrowseComponentsActivity.fetcher.serialized";
@@ -24,27 +20,21 @@ public class BrowseComponentsActivity extends FragmentActivity {
         if (Build.VERSION.SDK_INT >= 11) {
             onCreateAndroidSDK11AndUp();
         }
+    }
 
-        setContentView(R.layout.master_detail_base);
-
-        boolean showDetail = false;
-
-        if (savedInstanceState == null) {
-            Bundle args = new Bundle();
-            Fetcher fetcher;
-            if (getIntent().hasExtra(EXTRA_SERIALIZED_FETCHER)) {
-                fetcher = FetcherManager.unserializeFetcher(getIntent().getStringExtra(EXTRA_SERIALIZED_FETCHER));
-            } else {
-                fetcher = getIntent().getParcelableExtra(EXTRA_FETCHER);
-            }
-            args.putParcelable(BrowseComponentsFragment.ARG_FETCHER, fetcher);
-            BrowseComponentsFragment fragment = new BrowseComponentsFragment();
-            fragment.setArguments(args);
-            getSupportFragmentManager().beginTransaction().add(R.id.master, fragment).commit();
+    @Override
+    protected Fragment createMasterFragment() {
+        Bundle args = new Bundle();
+        Fetcher fetcher;
+        if (getIntent().hasExtra(EXTRA_SERIALIZED_FETCHER)) {
+            fetcher = FetcherManager.unserializeFetcher(getIntent().getStringExtra(EXTRA_SERIALIZED_FETCHER));
         } else {
-            showDetail = haveDetail();
+            fetcher = getIntent().getParcelableExtra(EXTRA_FETCHER);
         }
-        findViewById(R.id.detail).setVisibility(showDetail ? View.VISIBLE : View.GONE);
+        args.putParcelable(BrowseComponentsFragment.ARG_FETCHER, fetcher);
+        BrowseComponentsFragment fragment = new BrowseComponentsFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @TargetApi(11)
@@ -63,34 +53,4 @@ public class BrowseComponentsActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean haveDetail() {
-        return getSupportFragmentManager().findFragmentById(R.id.detail) != null;
-    }
-
-    private boolean usingTabletView() {
-        return getResources().getBoolean(R.bool.use_master_detail);
-    }
-
-    /**
-     * Open given fragment in detail panel or new activity
-     */
-    public void openFragment(Class<? extends Fragment> fragmentClass, Bundle arguments) {
-        if (usingTabletView()) {
-            Fragment fragment;
-            try {
-                fragment = fragmentClass.newInstance();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            fragment.setArguments(arguments);
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.detail, fragment)
-                    .commit();
-            findViewById(R.id.detail).setVisibility(View.VISIBLE);
-        } else {
-            arguments.putString(SingleFragmentActivity.EXTRA_FRAGMENT, fragmentClass.getName());
-            startActivity(new Intent(this, SingleFragmentActivity.class).replaceExtras(arguments));
-        }
-    }
 }
